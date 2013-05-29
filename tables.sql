8298da572c9c36c4ddaf6d40199ff5cb066e4076
Ivan Kozik <ivan@ludios.org>
2013-05-29 22:44:45 +0000

    Use ORDER BY to reduce query time from 1800ms to 50ms

--- tables.sql
+++ tables.sql
@@ -8,6 +8,8 @@ CREATE TABLE feeds (
 
 -- "this doesn't actually index individual array values, but instead indexes the entire array"
 -- http://stackoverflow.com/questions/4058731/can-postgresql-index-array-columns
+-- Confirmed to speed up job_ids = '{2}' and job_ids = '{}' queries.
+-- (Note: if LIMIT is used, must ORDER BY encoded_url)
 CREATE INDEX job_ids_idx ON feeds USING GIN ("job_ids");
 
 CREATE TABLE counters (
