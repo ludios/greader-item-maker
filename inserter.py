8d76642c28ad9a6eb9ef9111ed7516b1cf7fae43
Ivan Kozik <ivan@ludios.org>
2013-05-29 06:38:12 +0000

    Inserter: use quiet mode by default

--- inserter.py
+++ inserter.py
@@ -7,6 +7,7 @@ import sys
 import urllib
 
 py_ver = sys.version[0:3].replace(".", "")
+quiet = True
 
 def main():
 	try:
@@ -16,6 +17,8 @@ def main():
 	conn = psycopg2.connect("dbname='greader' user='greader' host='192.168.1.20' password='%s'" % (password,))
 	conn.set_isolation_level(ISOLATION_LEVEL_SERIALIZABLE)
 	cur = conn.cursor()
+	if quiet:
+		print "'#' means encoded URL was inserted; '.' means it was already inserted"
 	for feed_url in sys.stdin:
 		feed_url = feed_url.rstrip()
 		if not feed_url:
@@ -31,12 +34,18 @@ def main():
 			conn.commit()
 		except psycopg2.IntegrityError, e:
 			if 'duplicate key value violates unique constraint' in str(e):
-				print "%r already in table" % (encoded_url,)
+				if quiet:
+					print ".",
+				else:
+					print "%r already in table" % (encoded_url,)
 				conn.rollback()
 			else:
 				raise
 		else:
-			print "Inserted %r" % (encoded_url,)
+			if quiet:
+				print "#",
+			else:
+				print "Inserted %r" % (encoded_url,)
 
 if __name__ == '__main__':
 	main()
