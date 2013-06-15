ffb2be6b0f38a3e50cf123ab89211ad533c36e2c
Ivan Kozik <ivan@ludios.org>
2013-06-15 17:18:28 +0000

    Make dump_db take start key argument; add twitter-id-subset and twitter-username-subset

--- dump_db.py
+++ dump_db.py
@@ -8,8 +8,9 @@ from inserter_maker import open_db
 
 def main():
 	db_path = sys.argv[1]
+	start = sys.argv[2]
 	db = open_db(db_path)
-	for k, v in db.iterator().seekFirst():
+	for k, v in db.iterator().seek(start):
 		print repr(k), repr(v)
 	db.close()
 
