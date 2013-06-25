263bb8bed77ccc66a38c81ef2543970efcc76db4
Ivan Kozik <ivan@ludios.org>
2013-06-25 18:29:31 +0000

    Put some /paths in there so that unneeded URLs aren't grabbed

--- url_filter.py
+++ url_filter.py
@@ -10,36 +10,37 @@ FOURTH_SLASH, \
 SUBDOMAIN_AND_FIRST_SLASH, \
 FULL_URL = range(7)
 
-domain_to_action = {
+path_to_action = {
 	 'tumblr.com': SUBDOMAIN
 	,'community.livejournal.com': FIRST_SLASH
-	,'www.livejournal.com': SECOND_SLASH
+	,'www.livejournal.com/users': SECOND_SLASH
+	,'www.livejournal.com/community': SECOND_SLASH
 	,'livejournal.com': SUBDOMAIN
 	,'wordpress.com': SUBDOMAIN
 	,'blogspot.com': SUBDOMAIN
-	,'blogger.com': SECOND_SLASH
-	,'feeds.feedburner.com': SECOND_SLASH
-	,'feeds2.feedburner.com': SECOND_SLASH
+	,'blogger.com/feeds': SECOND_SLASH
+	,'feeds.feedburner.com': FIRST_SLASH
+	,'feeds2.feedburner.com': FIRST_SLASH
 	,'feeds.rapidfeeds.com': FIRST_SLASH
 	,'posterous.com': SUBDOMAIN
-	,'groups.google.com': SECOND_SLASH
-	,'groups.yahoo.com': SECOND_SLASH
+	,'groups.google.com/group': SECOND_SLASH
+	,'groups.yahoo.com/group': SECOND_SLASH
 	,'typepad.com': SUBDOMAIN_AND_FIRST_SLASH
 	,'typepad.jp': SUBDOMAIN_AND_FIRST_SLASH
 	,'blog.roodoo.com': FIRST_SLASH
 	,'diarynote.jp': SUBDOMAIN
 	,'ameblo.jp': FIRST_SLASH
 	,'rssblog.ameba.jp': FIRST_SLASH
-	,'wretch.cc': SECOND_SLASH
+	,'wretch.cc/blog': SECOND_SLASH
 	,'formspring.me': FIRST_SLASH
 	,'blog.shinobi.jp': SUBDOMAIN
-	,'rss.exblog.jp': THIRD_SLASH
+	,'rss.exblog.jp/rss/exblog': THIRD_SLASH
 	,'exblog.jp': SUBDOMAIN
 	,'blog.hexun.com': SUBDOMAIN
 	,'blog.hexun.com.tw': SUBDOMAIN
 	,'blog.livedoor.jp': FIRST_SLASH
 	,'altervista.org': SUBDOMAIN
-	,'feeds.qzone.qq.com': SECOND_SLASH
+	,'feeds.qzone.qq.com/cgi-bin': SECOND_SLASH
 	,'qzone.qq.com': SUBDOMAIN
 	,'blog.163.com': SUBDOMAIN
 	,'inube.com': SUBDOMAIN
@@ -47,7 +48,8 @@ domain_to_action = {
 	,'static.blog4ever.com': FULL_URL
 	,'www.xanga.com': FULL_URL
 	,'xanga.com': SUBDOMAIN
-	,'feed.pixnet.net': FOURTH_SLASH
+	,'feed.pixnet.net/blog/posts/rss': FOURTH_SLASH
+	,'feed.pixnet.net/blog/posts/atom': FOURTH_SLASH
 	,'pixnet.net': SUBDOMAIN
 	,'twitter.com': FIRST_SLASH
 	,'rss2lj.net': FULL_URL
@@ -59,7 +61,7 @@ domain_to_action = {
 	,'pipes.yahoo.com': FULL_URL
 	,'page2rss.com': FULL_URL
 	,'boards.4chan.org': FIRST_SLASH
-	,'dis.4chan.org': SECOND_SLASH
+	,'dis.4chan.org/atom': SECOND_SLASH
 	,'vox.com': SUBDOMAIN
 	,'jux.com': SUBDOMAIN
 	,'at.webry.info': SUBDOMAIN
@@ -73,10 +75,12 @@ domain_to_action = {
 # TODO: should *_SLASH get rid of ? and & params like href="/blog/kuni&rss20=1"
 
 def main():
-	if sys.argv[1:] == ["gimme_domains"]:
-		print "\n".join(domain_to_action.keys())
-		sys.exit(0)
+	for key in path_to_action.keys():
+		assert not key.endswith('/')
 
+	if sys.argv[1:] == ["print_paths"]:
+		print "\n".join(path_to_action.keys())
+		sys.exit(0)
 
 
 
